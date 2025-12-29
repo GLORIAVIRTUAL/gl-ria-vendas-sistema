c170321ceff7d8f48d2c77fd736a7c5eee51e7ce
Glória Virtual <dmpetrolina@gmail.com>
2025-12-29 13:14:23 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,79 +1,81 @@
-import Dashboard from './pages/Dashboard';
-import Agendar from './pages/Agendar';
-import Agendamentos from './pages/Agendamentos';
-import Negocios from './pages/Negocios';
-import NovoNegocio from './pages/NovoNegocio';
-import TesteStripe from './pages/TesteStripe';
 import APIDocumentacao from './pages/APIDocumentacao';
 import Afiliados from './pages/Afiliados';
-import CriarContaAfiliado from './pages/CriarContaAfiliado';
-import AgendamentoPublico from './pages/AgendamentoPublico';
-import CodigoEmbed from './pages/CodigoEmbed';
-import DashboardAfiliado from './pages/DashboardAfiliado';
-import CRMAfiliado from './pages/CRMAfiliado';
-import AgendarAfiliado from './pages/AgendarAfiliado';
-import AgendamentosAfiliado from './pages/AgendamentosAfiliado';
-import NovoNegocioAfiliado from './pages/NovoNegocioAfiliado';
-import NegociosAfiliado from './pages/NegociosAfiliado';
-import DisparosWhatsApp from './pages/DisparosWhatsApp';
-import ConfigurarCron from './pages/ConfigurarCron';
-import TesteWhatsApp from './pages/TesteWhatsApp';
-import ImportarProdutos from './pages/ImportarProdutos';
-import ConfigurarGoogleCalendar from './pages/ConfigurarGoogleCalendar';
-import DisparosEmail from './pages/DisparosEmail';
-import Clientes from './pages/Clientes';
-import ClientesAfiliado from './pages/ClientesAfiliado';
 import Agenda from './pages/Agenda';
 import AgendaAfiliado from './pages/AgendaAfiliado';
-import TesteWebhook from './pages/TesteWebhook';
+import AgendamentoPublico from './pages/AgendamentoPublico';
+import Agendamentos from './pages/Agendamentos';
+import AgendamentosAfiliado from './pages/AgendamentosAfiliado';
+import Agendar from './pages/Agendar';
+import AgendarAfiliado from './pages/AgendarAfiliado';
 import CRM from './pages/CRM';
-import OnboardingPublico from './pages/OnboardingPublico';
-import FormularioStart from './pages/FormularioStart';
-import Emails from './pages/Emails';
+import CRMAfiliado from './pages/CRMAfiliado';
 import ChatIA from './pages/ChatIA';
-import PipelineIA from './pages/PipelineIA';
-import DashboardIA from './pages/DashboardIA';
+import Clientes from './pages/Clientes';
+import ClientesAfiliado from './pages/ClientesAfiliado';
+import CodigoEmbed from './pages/CodigoEmbed';
 import ConfiguracoesIA from './pages/ConfiguracoesIA';
+import ConfigurarCron from './pages/ConfigurarCron';
+import ConfigurarGoogleCalendar from './pages/ConfigurarGoogleCalendar';
+import CriarContaAfiliado from './pages/CriarContaAfiliado';
+import Dashboard from './pages/Dashboard';
+import DashboardAfiliado from './pages/DashboardAfiliado';
+import DashboardIA from './pages/DashboardIA';
+import DisparosEmail from './pages/DisparosEmail';
+import DisparosWhatsApp from './pages/DisparosWhatsApp';
+import Emails from './pages/Emails';
+import FormularioStart from './pages/FormularioStart';
+import Home from './pages/Home';
+import ImportarProdutos from './pages/ImportarProdutos';
+import Negocios from './pages/Negocios';
+import NegociosAfiliado from './pages/NegociosAfiliado';
+import NovoNegocio from './pages/NovoNegocio';
+import NovoNegocioAfiliado from './pages/NovoNegocioAfiliado';
+import OnboardingPublico from './pages/OnboardingPublico';
+import PipelineIA from './pages/PipelineIA';
+import TesteStripe from './pages/TesteStripe';
+import TesteWebhook from './pages/TesteWebhook';
+import TesteWhatsApp from './pages/TesteWhatsApp';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Dashboard": Dashboard,
-    "Agendar": Agendar,
-    "Agendamentos": Agendamentos,
-    "Negocios": Negocios,
-    "NovoNegocio": NovoNegocio,
-    "TesteStripe": TesteStripe,
     "APIDocumentacao": APIDocumentacao,
     "Afiliados": Afiliados,
-    "CriarContaAfiliado": CriarContaAfiliado,
-    "AgendamentoPublico": AgendamentoPublico,
-    "CodigoEmbed": CodigoEmbed,
-    "DashboardAfiliado": DashboardAfiliado,
-    "CRMAfiliado": CRMAfiliado,
-    "AgendarAfiliado": AgendarAfiliado,
-    "AgendamentosAfiliado": AgendamentosAfiliado,
-    "NovoNegocioAfiliado": NovoNegocioAfiliado,
-    "NegociosAfiliado": NegociosAfiliado,
-    "DisparosWhatsApp": DisparosWhatsApp,
-    "ConfigurarCron": ConfigurarCron,
-    "TesteWhatsApp": TesteWhatsApp,
-    "ImportarProdutos": ImportarProdutos,
-    "ConfigurarGoogleCalendar": ConfigurarGoogleCalendar,
-    "DisparosEmail": DisparosEmail,
-    "Clientes": Clientes,
-    "ClientesAfiliado": ClientesAfiliado,
     "Agenda": Agenda,
     "AgendaAfiliado": AgendaAfiliado,
-    "TesteWebhook": TesteWebhook,
+    "AgendamentoPublico": AgendamentoPublico,
+    "Agendamentos": Agendamentos,
+    "AgendamentosAfiliado": AgendamentosAfiliado,
+    "Agendar": Agendar,
+    "AgendarAfiliado": AgendarAfiliado,
     "CRM": CRM,
-    "OnboardingPublico": OnboardingPublico,
-    "FormularioStart": FormularioStart,
-    "Emails": Emails,
+    "CRMAfiliado": CRMAfiliado,
     "ChatIA": ChatIA,
-    "PipelineIA": PipelineIA,
-    "DashboardIA": DashboardIA,
+    "Clientes": Clientes,
+    "ClientesAfiliado": ClientesAfiliado,
+    "CodigoEmbed": CodigoEmbed,
     "ConfiguracoesIA": ConfiguracoesIA,
+    "ConfigurarCron": ConfigurarCron,
+    "ConfigurarGoogleCalendar": ConfigurarGoogleCalendar,
+    "CriarContaAfiliado": CriarContaAfiliado,
+    "Dashboard": Dashboard,
+    "DashboardAfiliado": DashboardAfiliado,
+    "DashboardIA": DashboardIA,
+    "DisparosEmail": DisparosEmail,
+    "DisparosWhatsApp": DisparosWhatsApp,
+    "Emails": Emails,
+    "FormularioStart": FormularioStart,
+    "Home": Home,
+    "ImportarProdutos": ImportarProdutos,
+    "Negocios": Negocios,
+    "NegociosAfiliado": NegociosAfiliado,
+    "NovoNegocio": NovoNegocio,
+    "NovoNegocioAfiliado": NovoNegocioAfiliado,
+    "OnboardingPublico": OnboardingPublico,
+    "PipelineIA": PipelineIA,
+    "TesteStripe": TesteStripe,
+    "TesteWebhook": TesteWebhook,
+    "TesteWhatsApp": TesteWhatsApp,
 }
 
 export const pagesConfig = {
