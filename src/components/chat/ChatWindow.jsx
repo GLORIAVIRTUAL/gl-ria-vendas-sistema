1375fcb5958887293d3f7a61a0879278adfe9ca2
Glória Virtual <dmpetrolina@gmail.com>
2026-01-09 17:19:35 +0000

    File changes

--- src/components/chat/ChatWindow.jsx
+++ src/components/chat/ChatWindow.jsx
@@ -197,11 +197,14 @@ export default function ChatWindow({
         </div>
       </div>
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-1">
-        {messages.map((msg) => (
-          <MessageBubble key={msg.id} message={msg} />
-        ))}
-        <div ref={messagesEndRef} />
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        <TemplateSelector contact={contact} onSent={() => {}} />
+        <div className="space-y-1">
+          {messages.map((msg) => (
+            <MessageBubble key={msg.id} message={msg} />
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
       </div>
 
       {attachmentFile && (
